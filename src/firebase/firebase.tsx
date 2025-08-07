c417e7e9ea34dddcca2f56cfd33546b85d524dd3
MoisesVelez <moisesvelezfernandez@gmail.com>
2025-08-07 21:14:16 +0200

    Update Auth

--- src/firebase/firebase.tsx
+++ src/firebase/firebase.tsx
@@ -2,13 +2,15 @@ import {initializeApp} from 'firebase/app';
 import {getAuth} from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "<KEY>",
-    authDomain: "mova-2021.firebaseapp.com",
-    projectId: "mova-2021",
-    storageBucket: "mova-2021.appspot.com",
-    messagingSenderId: "1018442115919",
-    appId: "1:1018442115919:web:177117271217771217711"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
+
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
